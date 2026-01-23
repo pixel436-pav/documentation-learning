a581f1852eb7eb9f32b00401d3bc84baf8dbdaf5
pavneetSingh <pavneets436@gmail.com>
2026-01-23 18:32:56 +0530

    Importing 2 components from the same file

--- reactLearning/src/main.jsx
+++ reactLearning/src/main.jsx
@@ -1,10 +1,15 @@
+// Root Componrnt File  
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// importing another function from the same file
+import { Description } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
+    <Description/>
   </StrictMode>,
 )
