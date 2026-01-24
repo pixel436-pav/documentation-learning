1db8452a3599c28e4bb56fdf1d148831b0af316c
pavneetSingh <pavneets436@gmail.com>
2026-01-25 00:58:09 +0530

    Data as porps

--- reactLearning/src/Rendering lists/Render3.jsx
+++ reactLearning/src/Rendering lists/Render3.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Render3 = () => {
-    const fruits = [
-  { name: "apple", calories: 52 },
-  { name: "banana", calories: 89 },
-  { name: "orange", calories: 102 },
-  { name: "pineapple", calories: 100 }
-];
+const Render3 = (props) => {
+
 
   // fruits.sort((a,b) => { return  a.name.localeCompare(b.name) // alphabetical order   
   // fruits.sort((a,b) => { return b.name.localeCompare(a.name) // Reverse alphabetical order   
@@ -14,14 +10,19 @@ const Render3 = () => {
   // }
   // )
 
-  // const lowCalFruits = fruits.filter(lowCalFruit => { return lowCalFruit.calories < 100 }  ) - // for low cal fruits
-  const highCalFruits = fruits.filter(highCalFruit => { return highCalFruit.calories  >= 100 }  )
+  // // const lowCalFruits = fruits.filter(lowCalFruit => { return lowCalFruit.calories < 100 }  ) - // for low cal fruits
+  // const highCalFruits = fruits.filter(highCalFruit => { return highCalFruit.calories  >= 100 }  )
 
-    // here don't be confused with singular fruit WORD , We are just receiveing them here as a parameter here so no matter how we recieve it , it can be fruit or fruits or f or anything as just for the convinience we use fruit
-    const listItems = highCalFruits.map((highCalFruit,index) => {
-        return <li key={index}>{highCalFruit.name}:&nbsp;<b>{highCalFruit.calories}</b></li> // as we are passing index as key prop problem with it is that it sticks to the position not the object
+  //   // here don't be confused with singular fruit WORD , We are just receiveing them here as a parameter here so no matter how we recieve it , it can be fruit or fruits or f or anything as just for the convinience we use fruit
+  //   const listItems = highCalFruits.map((highCalFruit,index) => {
+  //       return <li key={index}>{highCalFruit.name}:&nbsp;<b>{highCalFruit.calories}</b></li> // as we are passing index as key prop problem with it is that it sticks to the position not the object
       
-    });
+  //   });
+
+  const itemList = props.items
+
+  const listItems = itemList.map(item=> { return <li> {item.name}: {item.calories}</li> })
+
 
   return (
     <ol>{listItems}</ol>
--- reactLearning/src/main.jsx
+++ reactLearning/src/main.jsx
@@ -9,7 +9,12 @@ import Render from './Rendering lists/Render.jsx'
 import Render2 from './Rendering lists/Render2.jsx'
 import Render3 from './Rendering lists/Render3.jsx'
 
-
+    const fruits = [
+  { name: "apple", calories: 52 },
+  { name: "banana", calories: 89 },
+  { name: "orange", calories: 102 },
+  { name: "pineapple", calories: 100 }
+];
 
 
 createRoot(document.getElementById('root')).render(
@@ -25,7 +30,7 @@ createRoot(document.getElementById('root')).render(
     <PackingList />
     <Render />
     <Render2 />
-    <Render3 />
+    <Render3 items={fruits} catrgory="Fruits"/>
   
   {/* // as i have passed these above values as props here now i can read them inside Avatar function inside its file */}
 
